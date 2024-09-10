d4549809d083afac5eb35af4138024faa44236ca
Redha <redha_moulla@yahoo.fr>
2024-09-11 01:05:05 +0200

    Add pandas sklearn

--- app.py
+++ app.py
@@ -1,57 +1,34 @@
 from flask import Flask, request, jsonify
-from prometheus_client import Counter, Summary, generate_latest
 import pickle
 import pandas as pd
-import time
-import logging
 
 app = Flask(__name__)
-
-# Configuration du logging
-logging.basicConfig(level=logging.INFO)
 
 # Charger le modèle de churn
 try:
     with open('churn_model.pkl', 'rb') as f:
         model = pickle.load(f)
-    logging.info("Modèle chargé avec succès")
 except Exception as e:
-    logging.error(f"Erreur lors du chargement du modèle: {e}")
-
-# Définir un compteur pour suivre le nombre total de prédictions
-PREDICTIONS_COUNTER = Counter('total_predictions', 'Nombre total de prédictions')
-# Résumé pour suivre le temps de traitement des requêtes
-REQUEST_TIME = Summary('request_processing_seconds', 'Temps de traitement des requêtes')
+    print(f"Erreur lors du chargement du modèle : {e}")
 
 @app.route('/predict', methods=['POST'])
-@REQUEST_TIME.time()  # Mesurer le temps de la requête
 def predict():
     try:
         data = request.get_json()
-        logging.info(f"Données reçues: {data}")
+        print(f"Données reçues : {data}")
         
-        # Les features doivent correspondre aux colonnes numériques utilisées lors de l'entraînement du modèle
+        # Convertir les données en DataFrame
         features = pd.DataFrame([data])
-
-        # Faire la prédiction à l'aide du modèle chargé
-        prediction = model.predict(features)[0]
-        logging.info(f"Prédiction: {prediction}")
         
-        # Incrémenter le compteur de prédictions
-        PREDICTIONS_COUNTER.inc()
-
+        # Prédire le churn à l'aide du modèle
+        prediction = model.predict(features)[0]
+        
         # Retourner la prédiction sous forme de réponse JSON
         result = {"prediction": "churn" if prediction == 1 else "no churn"}
         return jsonify(result)
     except Exception as e:
-        logging.error(f"Erreur lors de la prédiction: {e}")
-        return jsonify({"error": "Erreur lors du traitement de la prédiction"}), 500
-
-# Endpoint pour exposer les métriques Prometheus
-@app.route('/metrics')
-def metrics():
-    return generate_latest(), 200
+        print(f"Erreur lors de la prédiction : {e}")
+        return jsonify({"error": "Erreur lors de la prédiction"}), 500
 
 if __name__ == '__main__':
-    logging.info("Démarrage du serveur Flask")
     app.run(host='0.0.0.0', port=5000)
